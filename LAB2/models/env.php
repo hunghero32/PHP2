ca891ac259359e9c0db291579f41d17cc13ea093
hunghero32 <hung87800@gmail.com>
2024-01-17 19:44:25 +0700

    Buổi 7

--- LAB2/models/env.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // khai báo cấu hình csdl
-    const DB_NAME = "mvc";
-    const HOST = "127.0.0.1";// . localhost:10
-    const DB_USER = "root";
-    const DB_PASSWORD = "";
-    const DB_CHARSET = "utf8";
-?>
